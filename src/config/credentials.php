53bfe8338282e1648c53d897e1db687aa9394053
OlivierBarbier <olivier@1001menus.com>
2015-12-28 15:37:31 +0100

    update

---
+++ src/config/credentials.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'wsdl' => __DIR__ . '/../' . 'config/zuora.wsdl',
+    'endpoint' => 'https://apisandbox.zuora.com/apps/services/a/63.0',
+    'user' => 'olivier@1001menus.com',
+    'password' => 'olivier2@',
+ ];
+ 
